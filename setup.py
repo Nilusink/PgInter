05a77ac7b6997829155e1e4f7688c10fe831d210
nilusink <nilusink@protonmail.com>
2023-02-05 15:54:41 +0100

    fixed theme now included

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pginter",
-    version="0.0.1",
+    version="0.0.3",
     author="Nilusink",
     author_email="nilusink@protonmail.com",
     description="A python GUI interface, based on pygame",
@@ -23,4 +23,7 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.10",
+    data_files=[
+        ('default_theme', ['src/pginter/theme/themes/default.json'])
+    ]
 )
