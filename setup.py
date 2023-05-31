44539abb1231d72497f4c1f44a52ea4cf90e910e
nilusink <nilusink@protonmail.com>
2023-05-31 23:09:29 +0200

    fixed themes not being included. Frame image placement got some improvements as well

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pginter",
-    version="0.2.0",
+    version="0.2.1",
     author="Nilusink",
     author_email="nilusink@protonmail.com",
     description="A python GUI interface, based on pygame",
